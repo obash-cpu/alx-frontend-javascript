50ede8a668650467864fb51959cba8764d763b96
obash-cpu <absalomobala@gmail.com>
2023-07-27 19:40:14 +0300

    task

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-// initializeRooms.js
-const ClassRoom = require('./0-classroom.js');
+import ClassRoom from './0-classroom.js';
 
-function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  const rooms = roomSizes.map(size => new ClassRoom(size));
-  return rooms;
+
+export default initializeRooms() {
+	return [
+	new classRoom (19),
+	new classRoom (20),
+	new ClassRoom(34),
+	];
 }
-
-module.exports = initializeRooms;
