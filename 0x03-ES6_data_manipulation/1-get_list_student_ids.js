19bbf721b18194e6491faa238f61fa609fe6449a
obash-cpu <absalomobala@gmail.com>
2023-08-01 16:00:41 +0300

    task

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
-export default function getListStudentIds(arr) {
-	if (!Array.isArray(arr)) {
-    return [];
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
+    return list.map((obj) => obj.id);
   }
-  return arr.map((arr) => arr.id);
-
+  return [];
 }
